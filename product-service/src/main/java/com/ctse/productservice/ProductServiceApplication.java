cb4ac6738edec8c09c5d3e67f993f4195fb6cb75
Himasha Weerasinghe <himashatw@gmail.com>
2022-05-11 23:06:15 +0530

    log added

--- product-service/src/main/java/com/ctse/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/ctse/productservice/ProductServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Product Service Application Started");
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
