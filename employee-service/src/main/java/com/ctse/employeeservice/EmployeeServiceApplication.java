bede2f3258ebece02b37544cc7c2e19bc6a2bc11
Himasha Weerasinghe <himashatw@gmail.com>
2022-05-14 17:18:42 +0530

    annoatations changed

--- employee-service/src/main/java/com/ctse/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/ctse/employeeservice/EmployeeServiceApplication.java
@@ -2,11 +2,17 @@ package com.ctse.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Employee Service Application Started");
+
 		SpringApplication.run(EmployeeServiceApplication.class, args);
 	}
 
--- employee-service/src/test/java/com/ctse/employeeservice/EmployeeServiceApplicationTests.java
+++ employee-service/src/test/java/com/ctse/employeeservice/EmployeeServiceApplicationTests.java
@@ -1,13 +1,16 @@
 package com.ctse.employeeservice;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class EmployeeServiceApplicationTests {
+public class EmployeeServiceApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
