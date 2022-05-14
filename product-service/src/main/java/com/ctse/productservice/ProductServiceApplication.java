ebe2787629c11aa704b5c2c2d360334adf043524
Himasha Weerasinghe <himashatw@gmail.com>
2022-05-14 18:04:43 +0530

    annotations added

--- product-service/src/main/java/com/ctse/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/ctse/productservice/ProductServiceApplication.java
@@ -2,8 +2,12 @@ package com.ctse.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableSwagger2
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
