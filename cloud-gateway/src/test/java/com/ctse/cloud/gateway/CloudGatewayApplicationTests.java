a1ad3a73889d15526d5e9ff2653e1ca2cfa21a88
Himasha Weerasinghe <himashatw@gmail.com>
2022-05-15 23:03:58 +0530

    cloud gateway added

---
+++ cloud-gateway/src/test/java/com/ctse/cloud/gateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctse.cloud.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
